3c9c25bf09cd2a525b9c879c7794e479c4d11124
nareiqat <nabeelareiqat@gmail.com>
2023-11-24 00:00:09 -0500

    Day 20&21 out of 100 days of code - snake game

---
+++ snake.py
@@ -0,0 +1,66 @@
+from turtle import Screen, Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+           self.add_segment(position)
+
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # the first execution will make sure they are aligned
+            # starting when the first segment is moved by 20 after the first loop
+            # the second segment will move on top of the first and
+            # then the third will move to the old position of the second
+            # here is a demo
+            # Initially : 3 2 1 >> Nan 3 2 >> then becomes after line 37 : 3 2 1 this process repeats non stop
+            #                            1
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(270)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
+
+
+
+
+
